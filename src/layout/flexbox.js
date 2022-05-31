0348e34e1a0a3008ec5894e31dc75575617cd26f
turtle601 <turtle601@naver.com>
2022-05-31 10:22:16 +0900

    Issue #06 feature: flexbox layout 디자인 셋팅

---
+++ src/layout/flexbox.js
@@ -0,0 +1,21 @@
+import { css } from 'styled-components';
+
+export const Flex = css`
+  display: flex;
+`;
+
+export const Center = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const hCenter = css`
+  display: flex;
+  justify-content: center;
+`;
+
+export const vCenter = css`
+  display: flex;
+  align-items: center;
+`;
