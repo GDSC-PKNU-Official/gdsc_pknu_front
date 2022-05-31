955bdfee8ee1adeaa77ba2f1f13a68b5feabd346
turtle601 <turtle601@naver.com>
2022-05-31 14:31:40 +0900

    Issue#06 feature : 커스텀 Button 컴포넌트 생성

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+import { Link } from 'react-router-dom';
+
+import { color } from '../../styles/color';
+
+const btnStyle = css`
+  // default 값
+  border: none;
+  outline: none;
+  cursor: pointer;
+
+  width: max-content;
+  height: max-content;
+
+  background-color: #c4c4c4;
+  color: ${color.white};
+
+  border-radius: 20px;
+
+  // button 속성 스타일
+
+  // 넓이
+  ${({ w }) => {
+    return (
+      w &&
+      css`
+        width: ${w};
+      `
+    );
+  }}
+
+  // 높이
+  ${({ h }) => {
+    return (
+      h &&
+      css`
+        height: ${h};
+      `
+    );
+  }}
+
+  // 색깔
+  ${({ color }) => {
+    return (
+      color &&
+      css`
+        background-color: ${color};
+      `
+    );
+  }}
+
+  // 버튼 스타일
+  ${({ variant }) => {
+    switch (variant) {
+      case 'solid':
+        return css`
+          background-color: ${props.color ? props.color : '#c4c4c4'};
+        `;
+
+      case 'outline':
+        return css`
+          background-color: ${color.white};
+          border: 1px solid ${props.color ? props.color : '#c4c4c4'};
+          color: ${props.color ? props.color : '#c4c4c4'};
+        `;
+
+      case 'ghost':
+        return css`
+          background-color: ${color.white};
+          color: ${props.color ? props.color : '#c4c4c4'};
+        `;
+
+      default:
+        return;
+    }
+  }}
+`;
+
+const StyledButton = styled.button`
+  ${btnStyle}
+`;
+
+const StyledLinkButton = styled(Link)`
+  ${btnStyle}
+`;
+
+function Button(props) {
+  return props?.variant === 'link' ? <StyledLinkButton {...props}></StyledLinkButton> : <StyledButton {...props}></StyledButton>;
+}
+
+export default Button;
