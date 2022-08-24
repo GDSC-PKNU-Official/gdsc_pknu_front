67df5ec78e307716152c11398f14e3cf62e66624
pp449 <mma7710@naver.com>
2022-08-24 19:19:50 +0900

    Issue #33 refactor: Change Testpage's GDSC LOGO, it can seperate color

--- src/components/mainpage/testpage/Testpage.jsx
+++ src/components/mainpage/testpage/Testpage.jsx
@@ -14,15 +14,12 @@ const Tmp = styled.div`
 
 const Div = styled.div`
     display:block;
+    width: 300px;
 `;
 
-const Img = styled.img`
-    width: 300px;
-    height: 300px;
-    .cls-1{
-        fill: black;
-    }
-`;
+// const Img = styled.img`
+//     width: 300px;
+// `;
 
 const Text = styled.p`
     font-family: 'googleSansDisplayRegular';
@@ -32,22 +29,33 @@ const Text = styled.p`
     }};
 `;
 
+const SVG = props => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 121.12 58.6"><defs></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1">
+            <path d="M28.84,29.42,48.5,18.06A9.67,9.67,0,1,0,38.83,1.3l-34,19.62Z" fill={props.color1} />
+            <path d="M43.66,58.6A9.68,9.68,0,0,0,48.5,40.54l-34-19.62A9.68,9.68,0,0,0,4.84,37.68l34,19.62A9.63,9.63,0,0,0,43.66,58.6Z" fill={props.color2} />
+            <path d="M77.46,58.6a9.6,9.6,0,0,0,4.83-1.3l34-19.62L92.59,29l-20,11.53A9.68,9.68,0,0,0,77.46,58.6Z" fill={props.color3}/>
+            <path d="M111.43,39a9.68,9.68,0,0,0,4.85-18.06L82.29,1.3a9.67,9.67,0,1,0-9.67,16.76l34,19.62A9.66,9.66,0,0,0,111.43,39Z" fill={props.color4}/>
+        </g></g></svg>
+    )
+}
+
 function TestPage() {
     return (
         <Tmp>
             <Div>
-                <Img src={process.env.PUBLIC_URL + '/assets/logo/GDSC_Intro.svg'} alt="GDSCpknu 로고" />
+                <SVG color1="grey" color2="black" color3="#fbbc04" color4="#34a853"></SVG>
             </Div>
-            <Text color={color.gdscRed}>
+            <Text color={color.gdsc.red}>
                 G
             </Text>
-            <Text color={color.gdscBlue}>
+            <Text color={color.gdsc.blue}>
                 D
             </Text>
-            <Text color={color.gdscGreen}>
+            <Text color={color.gdsc.green}>
                 S
             </Text>
-            <Text color={color.gdscYellow}>
+            <Text color={color.gdsc.yellow}>
                 C
             </Text>
         </Tmp>
