9f22fdbf47c99002b02d97152878ea042ede6423
Jiyu215 <jinyu010512@gmail.com>
2022-08-27 17:48:03 +0900

    Issue #21 feat: Create slide function
    -Add and edit layouts to create slides
    -Create a move layout function when clicked to add a slide function

--- src/components/mainpage/thirdpage/ThirdPageCard.jsx
+++ src/components/mainpage/thirdpage/ThirdPageCard.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React,{useState, useRef} from "react";
 import styled from "styled-components";
 import space from "../../../styles/space";
 import { color } from "../../../styles/color";
 import { fontSizes,fontWeights } from "../../../styles/font";
+import { useEffect } from "react";
 
-const ThirdPageCardItem = styled.div`
-    display:flex;
-    flex-direction: column;
+const ThirdPageCardItem = styled.li`
+    display:inline-block;
+    list-style:none;
+    white-space: normal;
     width:30%;
-    height:350px;
+    height:100%;
+    margin:${space['5']};
     overflow:hidden;
     border-radius:5%;
-    box-shadow:0px 1px 5px 1px ${color.gdsc.blue};
+    box-shadow: rgba(66, 133, 244, 0.16) 0px 3px 6px, rgba(66, 133, 244, 0.23) 0px 3px 6px;
 `; 
 
 const ThirdPageCardText = styled.p`
@@ -28,35 +31,98 @@ const ThirdPageCardTextTitle = styled.h4`
     font-weight:${fontWeights.bold};
 `;
 
+const ThirdPageCardItemGroup = styled.div`
+    width:100%;
+    display:flex;
+    justify-content: center;
+    overflow:hidden;
+`;
+const ThirdPageCardSlideGroup = styled.ul`
+    width:100%;
+    height:100%;
+    white-space: nowrap;
+    padding:0;
+`;
+
 const ThirdPageCardImg = styled.img`
     width:100%;
-    height:200px;
+    height:80%;
 `;
 
 const GDSC_GROWTH = [
     {
         id: "TechTalk",
         title: "테크톡",
-        text: "GDSC PKNU 자체의 기술세미나"
+        text: "GDSC PKNU 자체의 기술세미나",
+        img: "/assets/img/테크톡.jpg"
     },
     {
         id: "Codelab",
         title: "코드랩",
-        text: "프로그래밍 언어를 단계적으로 공부 및 실습"
+        text: "프로그래밍 언어를 단계적으로 공부 및 실습",
+        img: "/assets/img/테크톡.jpg"
     },
     {
         id: "Conference",
         title: "컨퍼런스",
-        text: "전문적인 주제에 대한 토론 및 발표"
+        text: "전문적인 주제에 대한 토론 및 발표",
+        img: "/assets/img/테크톡.jpg"   
+    },
+    {
+        id: "4",
+        title: "컨퍼런스",
+        text: "전문적인 주제에 대한 토론 및 발표",
+        img: "/assets/img/테크톡.jpg"   
+    },
+    {
+        id: "5",
+        title: "코드랩",
+        text: "프로그래밍 언어를 단계적으로 공부 및 실습",
+        img: "/assets/img/테크톡.jpg"   
+    },
+    {
+        id: "6",
+        title: "컨퍼런스",
+        text: "전문적인 주제에 대한 토론 및 발표",
+        img: "/assets/img/테크톡.jpg"   
+    },
+    {
+        id: "7",
+        title: "컨퍼런스",
+        text: "전문적인 주제에 대한 토론 및 발표",
+        img: "/assets/img/테크톡.jpg"   
     }
 ];
+const ButtonGroup = styled.div`
+    display:flex;
+    justify-content: center;
+`;
 
-const growthCards = () => {
-    const growthCard = GDSC_GROWTH.map(growth => {
-        const source = `/assets/img/${growth.title}.jpg`;
+const SliderButton = styled.button`
+    font-size:${fontSizes['3xl']};
+    border-radius: 20%;
+    width:${fontSizes['6xl']};  
+    height:${fontSizes['6xl']};
+    margin:0 ${space[4]};
+    color:#575757;
+    box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.28) 0px 0px 0px 1px;  
+    border:none;
+    font-weight:${fontWeights.bold};
+    background:none;
+    cursor:pointer;
+    &:hover{
+        cursor:pointer;
+        background:#575757;
+        color:${color.white};
+        transition : all 0.1s ease-in-out;
+    }
+`;
+
+function GrowthCards () {
+    const growthCard = GDSC_GROWTH.map(growth => {    
         return(
             <ThirdPageCardItem key={growth.id}>
-                <ThirdPageCardImg src={source} alt={growth.title}/>
+                <ThirdPageCardImg src={growth.img} alt={growth.title}/>
                 <ThirdPageCardText>
                     <ThirdPageCardTextTitle>{growth.title}</ThirdPageCardTextTitle>
                     {growth.text}
@@ -66,10 +132,44 @@ const growthCards = () => {
     })
     return growthCard;
 }
-function ThirdPageCard(){
+
+function ThirdPageCard(){ 
+    const GDSC_GROWTH_CLICK = GDSC_GROWTH.length-3;
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const slideRef = useRef(null);
+
+    const NextSlide = () =>{
+        if(currentSlide>=GDSC_GROWTH_CLICK){
+            setCurrentSlide(currentSlide);
+        }else{
+            setCurrentSlide(currentSlide+1);
+        }
+    };
+
+    const PrevSlide = () => {
+        if(currentSlide === 0){
+            setCurrentSlide(0);
+        }else{
+            setCurrentSlide(currentSlide-1);
+        }
+    }
+
+    useEffect(()=>{
+        slideRef.current.style.transition = 'all 0.5s ease-in-out';
+        slideRef.current.style.transform = `translateX(-${currentSlide*32}%)`;   
+    },[currentSlide]);
+
     return(
             <>
-                {growthCards()}
+                <ThirdPageCardItemGroup>
+                    <ThirdPageCardSlideGroup ref={slideRef}>
+                    <GrowthCards/>
+                    </ThirdPageCardSlideGroup>
+                </ThirdPageCardItemGroup>
+                <ButtonGroup>
+                    <SliderButton onClick={PrevSlide}>&lt;</SliderButton>
+                    <SliderButton onClick={NextSlide}>&gt;</SliderButton>
+                </ButtonGroup>
             </>
         )
 }
